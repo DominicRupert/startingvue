c4229da64d50d233429d45e8e7107a2b0ce43a92
dom <dominicrupert@gmail.com>
2022-04-26 17:19:59 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+      <h1>{{ name }}</h1>
+    <router-view />
+  </main>
+  <footer>
+    <div class="bg-dark text-light text-center p-4">
+    </div>
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+export default {
+  name: '',
+  setup() {
+    return {
+      name: "Dom",
+      appState: computed(() => AppState)
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+</style>
